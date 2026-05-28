4c2d9d4131fe3bb479d43ff07df6079c89476727
anishkumar51555 <anishkumar51555@gmail.com>
2026-05-28 16:35:18 +0530

    Prepared for deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://expense-splitter-backend-2q5v.onrender.com",
+  baseURL: "https://expense-splitter-backend-2q5v.onrender.com/api",
 });
 
 // 🔥 TOKEN AUTO ATTACH
